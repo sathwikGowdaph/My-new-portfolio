05f865ea88440b90cc43bde8677d7a2e4701b61e
sathwikGowdaph <sathwikgowdaph@gmail.com>
2026-01-23 20:49:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const toggle=document.getElementById("themeToggle")
-toggle.onclick=()=>{document.body.classList.toggle("dark-mode")}
-
-new Typed("#typed-name",{
-strings:["Sathwik Gowda P H","Frontend Developer","UI/UX Designer"],
+new Typed("#typed",{
+strings:["Creative Designer","Sathwik Gowda P H"],
 typeSpeed:70,
 backSpeed:40,
 loop:true
 })
 
-const bars=document.querySelectorAll(".chart-bar-fill")
-window.addEventListener("scroll",()=>{
-bars.forEach(bar=>{bar.style.width=bar.style.getPropertyValue("--width")})
+const toggle=document.getElementById("themeToggle")
+
+if(localStorage.getItem("theme")==="dark"){
+document.body.classList.add("dark-mode")
+toggle.textContent="☀️"
+}
+
+toggle.addEventListener("click",()=>{
+document.body.classList.toggle("dark-mode")
+if(document.body.classList.contains("dark-mode")){
+toggle.textContent="☀️"
+localStorage.setItem("theme","dark")
+}else{
+toggle.textContent="🌙"
+localStorage.setItem("theme","light")
+}
 })
